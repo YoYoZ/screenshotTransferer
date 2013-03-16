a783b9083ed9e8808424cc0bc27cc142064a6ce0
YoYoZ <yoyoza5@gmail.com>
2013-03-16 18:02:01 +0200

    First commit

---
+++ screeenshotSender/screeenshotSender/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace screeenshotSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            #warning Удалить следующие две строки, когда закончим.
+            Form2 fr = new Form2();
+            fr.Show();
+        }
+
+        public void generateSettings()
+        {
+            FileStream fs = new FileStream(Directory.GetCurrentDirectory()+@"/settings.cfg", FileMode.CreateNew, FileAccess.Write);
+            StreamWriter sr = new StreamWriter(fs);
+            sr.WriteLine("folder=" + Directory.GetCurrentDirectory());
+            sr.WriteLine("alwaysDisplay=true");
+            sr.WriteLine("timeout=2");
+          
+        }
+        private void connect()
+        { 
+        }
+        
+
+    }
+}
---
+++ screeenshotSender/screeenshotSender/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace screeenshotSender
+{
+    public partial class Form2 : Form
+    {
+        private List<string[]> friends;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void selectFolder(object sender, EventArgs e)
+        {
+           FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            textBox1.Text = fbd.SelectedPath;
+        }
+
+        private void addFriends()
+        {
+            friends.Add(string[] = {"name", "ip"});
+        }
+        private void saveSettings()
+        {
+
+        }
+    }
+}
